711cc1cf27e02172fe2d0bd6fbc0d51a926359b4
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-10-15 16:29:22 +0200

    Created class AlarmClock, made basic member structure

---
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L02A
+{
+    public class AlarmClock
+    {
+        #region Fields
+        private int _hour;
+        private int _minute;
+        private int _alarmHour;
+        private int _alarmMinute; 
+        #endregion
+
+
+        #region Properties
+        public int Hour
+        {
+            get { return _hour; }
+            set { _hour = value; }
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set { _minute = value; }
+        }
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set { _alarmHour = value; }
+        }
+
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set { _alarmMinute = value; }
+        } 
+        #endregion
+
+
+        #region Constructors
+        public AlarmClock()
+        {
+
+        }
+        public AlarmClock(int hour, int minute)
+        {
+
+        }
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+
+        } 
+        #endregion
+
+        public bool TickTock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
