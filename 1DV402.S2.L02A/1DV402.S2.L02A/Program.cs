69921b499e66ef4eeefabf2d8a5c3e0942352d48
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-10-15 17:54:01 +0200

    Started class Main, basic structure built. Methods Run(), ViewErrorMessage() and ViewTestHeader() coded.

--- 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
@@ -8,8 +8,28 @@ namespace _1DV402.S2.L02A
 {
     class Program
     {
+        static string horizontalLine = new String('=', 80);
+
         static void Main(string[] args)
         {
+            
+            //Console.WriteLine(HorizontalLine);
+        }
+        private static void Run(AlarmClock ac, int minutes)
+        {
+            ac.Minute = ac.Minute + minutes;
+        }
+
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static void ViewTestHeader(string header)
+        {
+            Console.WriteLine("{0}\n{1}", horizontalLine, header);
         }
     }
 }
